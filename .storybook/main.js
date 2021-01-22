c77c189aa1ecd3cac9afb877a8d008b21491a7b9
Erik van Dam <erikvandam93@gmail.com>
2021-01-22 18:26:52 +0100

    Added Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,36 @@
+const { join } = require('path')
+
+const toPath = (path) => join(process.cwd(), path)
+
+module.exports = {
+  stories: [
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: true,
+        backgrounds: true,
+        controls: true,
+        docs: true,
+        viewport: true,
+        toolbars: true,
+      },
+    },
+  ],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@packages/utils': toPath('./packages/utils'),
+        },
+      },
+    }
+  },
+}
