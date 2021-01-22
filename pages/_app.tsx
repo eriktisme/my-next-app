2696da3d31895de997fc60d73f8f50fc678230ba
Erik van Dam <erikvandam93@gmail.com>
2021-01-22 13:40:18 +0100

    Added Tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
+import fetch from "../packages/fetch";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Component { ...pageProps } />
+  )
 }
 
 export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './components/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
